53ca70f12abb94aab5a61a844c5bee3c1e498c56
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-05-21 22:56:07 +0000

    Localisation updates. Update credits.

--- NewestPages.i18n.php
+++ NewestPages.i18n.php
@@ -564,7 +564,7 @@ $messages['pt'] = array(
 );
 
 /** Russian (Русский)
- * @author .:Ajvol:.
+ * @author Александр Сигачёв
  */
 $messages['ru'] = array(
 	'newestpages'            => 'Новые страницы',
