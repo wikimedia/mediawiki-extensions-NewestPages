9fcdee3ee84a2b74d743193c7f20fc5bf32861d9
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-07 23:29:59 +0000

    * Fix a lot of extension special pages mess. Less messy now, aside from the naming of the files containing the aliases. That's for next time. * Add special page aliases for AbuseFilter * Change special page aliases method for ChangeAuthor, and Collection * Add missing to Translate/aliases.txt
    (may require another commit; we will see)

--- NewestPages.alias.php
+++ NewestPages.alias.php
@@ -11,12 +11,12 @@ $aliases = array();
  * @author Jon Harald Søby
  */
 $aliases['en'] = array(
-    'NewestPages'            => array('NewestPages')
+	'NewestPages'            => array('NewestPages')
 );
 
 /** Norwegian (Norsk (bokmål))
  * @author Jon Harald Søby
  */
 $aliases['no'] = array(
-    'NewestPages'            => array('Siste_sider')
+	'NewestPages'            => array('Siste_sider')
 );
