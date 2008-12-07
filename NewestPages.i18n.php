26983c3870ccaae40012f7649bfd6d5c36a91ece
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-12-07 00:08:04 +0000

    Localisation updates for extension messages from Betawiki (2008-12-07 00:49 CET)

--- NewestPages.i18n.php
+++ NewestPages.i18n.php
@@ -818,15 +818,17 @@ $messages['ps'] = array(
 
 /** Portuguese (Português)
  * @author Lugusto
+ * @author Waldir
  */
 $messages['pt'] = array(
 	'newestpages' => 'Páginas novas',
-	'newestpages-header' => "'''Esta página lista as $1 páginas mais recentes deste wiki.'''",
+	'newestpages-desc' => 'Mostra as [[Special:Newestpages|últimas X páginas]] adicionadas ao wiki',
+	'newestpages-header' => "'''Esta página lista {{PLURAL:$1|a página mais recente|as $1 páginas mais recentes}} deste wiki.'''",
 	'newestpages-limitlinks' => 'Exibir até $1 páginas',
 	'newestpages-namespace' => 'Espaço nominal:',
 	'newestpages-none' => 'Não foram encontrados resultados.',
-	'newestpages-ns-header' => "'''Esta página lista as $1 páginas mais recentes do espaço nominal $2.'''",
-	'newestpages-showing' => 'Listando as $1 páginas mais recentes:',
+	'newestpages-ns-header' => "'''Esta página lista {{PLURAL:$1|a página mais recente|as $1 páginas mais recentes}} do espaço nominal $2.'''",
+	'newestpages-showing' => 'Listando {{PLURAL:$1|a página mais recente|as $1 páginas mais recentes}}:',
 	'newestpages-submit' => 'Ir',
 	'newestpages-showredir' => 'Exibir redireccionamentos',
 	'newestpages-hideredir' => 'Ocultar redireccionamentos',
